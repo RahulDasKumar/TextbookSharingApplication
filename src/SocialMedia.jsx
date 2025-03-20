8e9062223a5a9c0334694865fdae65fd57f2692a
Alexander Karendys <akarendy@uncc.edu>
2025-03-20 16:03:23 -0400

    Added Social Media Content

--- src/SocialMedia.jsx
+++ src/SocialMedia.jsx
@@ -11,37 +11,48 @@ import {
 } from "./components/ui/carousel"
 
 export default function SocialMedia() {
+    const images = [
+        { src: "/images/facebook.png", alt: "Facebook" },
+        { src: "/images/youtube.png", alt: "YouTube" },
+        { src: "/images/instagram.png", alt: "Instagram" },
+    ]
+
     return (
         <>
         <section className="w-[100vw] h-[40vh]"></section>
-        <section className="flex flex-row justify-center">
+        <section className="flex flex-row justify-center scale-50">
         <Carousel className="w-full max-w-sm"
                     plugins={[
-                        AutoScroll({
-                        }),
+                        AutoScroll({}),
                     ]}
                     opts={{
                         loop: true
                     }}
-        
         >
             <CarouselContent className="-ml-1">
-                {Array.from({ length: 5 }).map((_, index) => (
-                    <CarouselItem key={index} className="pl-1 md:basis-1/2 lg:basis-1/3">
-                        <div className="p-1">
-                            <Card>
-                                <CardContent className="flex aspect-square items-center justify-center p-6">
-                                    <span className="text-2xl font-semibold">Social Media</span>
-                                </CardContent>
-                            </Card>
-                        </div>
-                    </CarouselItem>
-                ))}
+                {Array.from({ length: 5 }).map((_, index) => {
+                    const image = images[index % images.length]
+                    return (
+                        <CarouselItem key={index} className="pl-1 md:basis-1/2 lg:basis-1/3">
+                            <div className="p-1">
+                                <Card>
+                                    <CardContent className="flex aspect-square items-center justify-center p-3">
+                                        <img
+                                            src={image.src}
+                                            alt={image.alt}
+                                            className="w-16 h-16 object-contain"
+                                        />
+                                    </CardContent>
+                                </Card>
+                            </div>
+                        </CarouselItem>
+                    )
+                })}
             </CarouselContent>
             <CarouselPrevious />
             <CarouselNext />
         </Carousel>
-            </section>
+        </section>
         </>
     )
 }
